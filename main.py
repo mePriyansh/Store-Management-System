8fc3ba912a7fd40f525148a1fe040c0591299ee2
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-10-04 11:28:34 +0530

    Decorator used

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     pay_rate = 0.8 #Class attribute
     all=[]
@@ -20,15 +22,20 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
         
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=int(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
+        
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
-
-for Instance in Item.all:
-    print(Instance.name)
+Item.instantiate_from_csv()
+print(Item.all)
