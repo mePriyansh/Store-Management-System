4669630e77dd04a76c29ccec8e5a624075b0b506
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-10-04 09:12:13 +0530

    all list created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 #Class attribute
+    all=[]
     
     def __init__(self, name: str, price: float ,quantity=0):
         #Run validation to the received arguments
@@ -10,6 +11,8 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        
+        Item.all.append(self)
     
     def calculate_total_price(self):
         return self.price*self.quantity
@@ -17,15 +20,12 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
-print(Item.pay_rate) #To print the class attribute
 
-item1=Item("Phone",100,5)
-print(item1.__dict__) #To print all the attributes of the object 
-item1.apply_discount()
-print(item1.price)
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-item2=Item("Laptop",1000,3)
-item2.pay_rate=0.7
-print(item2.__dict__)
-item2.apply_discount()
-print(item2.price)
+for Instance in Item.all:
+    print(Instance.name)
