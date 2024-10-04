b77292ee821a360375feaa752a252849c0689c0e
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-10-04 19:53:17 +0530

    Encapsulaton

--- item.py
+++ item.py
@@ -20,6 +20,14 @@ class Item:
     def name(self):
         return self.__name
     
+    @name.setter
+    #Setter property
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception("The name is too long!")
+        else:
+            self.__name = value
+    
     def calculate_total_price(self):
         return self.price*self.quantity
     
